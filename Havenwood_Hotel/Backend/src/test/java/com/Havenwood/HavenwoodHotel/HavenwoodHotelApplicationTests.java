8135aef73d9dcd8ad535ca1fe460752855a2381c
Vikram18s <vikrams4114@gmail.com>
2025-07-23 15:00:35 +0530

    Created packages

---
+++ Havenwood_Hotel/Backend/src/test/java/com/Havenwood/HavenwoodHotel/HavenwoodHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Havenwood.HavenwoodHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenwoodHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
